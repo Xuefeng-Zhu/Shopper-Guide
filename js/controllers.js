c95222d865543d5020e73871e20b7a77d7b42fb3
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-09-20 14:25:45 -0500

    add side bar

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,19 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MainCtrl', ["$scope", function($scope) {
+  controller('MainCtrl', ['$scope', function($scope) {
+  	$scope.items = []
+
+  	$scope.$watch('quantity', function(){
+  		if ($scope.items.length < $scope.quantity){
+  			var count = $scope.quantity -  $scope.items.length
+  			for (var i = 0; i < count; i++){
+  				$scope.items.push({category: ''});
+  			}
+  		}
+  		else{
+  			$scope.items = $scope.items.slice(0, $scope.quantity);
+  		}
+  	});
 
   }]);
